afc5815668fed0efb014a5d99c7da7303fc034ec
Kyle Honeycutt <coinables@gmail.com>
2017-09-09 09:19:30 -0700

    change from btc-e to coinbase

--- 186.php
+++ 186.php
@@ -1,7 +1,7 @@
 <?php
-$url = "https://btc-e.com/api/2/btc_usd/ticker";
+$url = "https://coinbase.com/api/v1/prices/spot_rate";
 $json = json_decode(file_get_contents($url), true);
-$price = $json["ticker"]["last"];
+$price = $json["amount"];
 $usdPrice = 20; // SET THE PRICE OF THE ITEM HERE
 $calc = $usdPrice / $price;
 $itemPrice = round($calc, 4);
